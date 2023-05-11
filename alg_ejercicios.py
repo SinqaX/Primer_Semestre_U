e9a719aeb3ccbc85e359c529b675755d110956d3
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-05-11 09:47:59 -0500

    U alg_ejercicios.py

--- alg_ejercicios.py
+++ alg_ejercicios.py
@@ -423,31 +423,30 @@ while cn <= cd:
 
 print("La cantidad de números primos es", cp, "y la cantidad de múltiplos de 3 es", ct, "y las tablas de multiplicar de números pares entre estos dos números son:")
 if cp < ct:
-    ctm = cp
+    ctm == cp
     
     while ctm <= ct:
-         if ctm % 2 == 0: 
-          print("TABLA DEL", ctm)
-          while xt < 11:
-            r = ctm * xt
-            print(ctm, "x", xt, "=", r)
-            xt = xt + 1
-            
-         ctm = ctm + 1
-         xt = 1
+        if ctm % 2 == 0:
+            print("TABLA DEL : ",ctm)
+            while xt < 11:
+              r = ctm * xt
+              print(ctm, "x", xt, "=", r)
+              xt = xt + 1
+        ctm = ctm +1
+        xt = 1
+        
 else:
-    ctm = ct
+    ctm == ct
     
     while ctm <= cp:
         if ctm % 2 == 0:
-         print("TABLA DEL", ctm)
-         while xt < 11:
-          r = ctm * xt
-          print(ctm, "x", xt, "=", r)
-          xt = xt + 1
-            
-         ctm = ctm + 1
-         xt = 1
+            print("TABLA DEL : ",ctm)
+            while xt < 11:
+              r = ctm * xt
+              print(ctm, "x", xt, "=", r)
+              xt = xt + 1
+        ctm = ctm +1
+        xt = 1
 
 
 
