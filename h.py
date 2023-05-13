312e0ce8b0656776722635c3e4109c7da4adc1ff
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-05-13 18:33:18 -0500

    Create h.py

---
+++ h.py
@@ -0,0 +1,30 @@
+numero_uno = 7
+numero_dos = 7
+resultado = numero_uno + numero_uno
+resultado = str(resultado)
+print("el resultado de la suma es: " + resultado)
+
+
+mensaje ="hola_sebastian"
+buscar_subcadena = mensaje.find("tian")
+print(buscar_subcadena)
+
+mensaje ="hola_sebastian"
+extraer_subcadena = mensaje[2:9]
+print(extraer_subcadena)
+
+mensaje_uno = "hola"
+mensaje_dos = "hola"
+print(mensaje_uno == mensaje_dos)
+
+""" """
+
+
+
+
+
+
+
+
+
+
