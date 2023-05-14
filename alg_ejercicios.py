f580028fb7403ebc17e44cb344835cf13553c4ab
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-05-13 20:28:57 -0500

    Update alg_ejercicios.py

--- alg_ejercicios.py
+++ alg_ejercicios.py
@@ -451,3 +451,31 @@ else:
 
 
 
+#PROMEDIO PRIMOS
+cn=int(input("ingrese la cantidad de numeros : "))
+nv=1
+cp=0
+sp=0
+ce=0
+c=1
+while nv<=cn:
+    num=int(input("ingrese el numero : "))
+    ce=0
+    while c<=num:
+        if num%c==0:
+            ce=ce+1
+        c=c+1
+
+    if ce==2:
+        cp=cp+1
+        sp=sp+num
+    nv=nv+1
+    c=1
+
+if cp>0:
+    pp=sp/cp
+    print(sp)
+    print(cp)
+    print("el promedio de numeros pares es : ",pp)
+else:
+    print("no hay numeros pares en los numeros ingresados")
