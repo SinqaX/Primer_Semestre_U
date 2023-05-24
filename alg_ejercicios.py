ca07a0ed0e8f091b9892962860fc80d8a0ec96df
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-05-23 22:12:42 -0500

    Update alg_ejercicios.py

--- alg_ejercicios.py
+++ alg_ejercicios.py
@@ -978,3 +978,146 @@ else:
                 ls=ls+1
         ct=ct+1
         r=0
+
+
+
+
+
+#EJERCICIO SEGUNDO Y CUARTO PRIMO CONSECUTIVO
+cd=int(input("ingrese la cantidad de numeros : "))
+nv=1
+b=0
+c=2
+cp=0
+l=0
+zp=0
+sp=0
+ap=0
+while nv<=cd:
+    num=int(input("ingrese el numero : "))
+    while c<num and b==0:
+        if num%c==0:
+            b=0
+        else:
+            b=1
+        c=c+1
+    if b==0:
+        cp=cp+1
+    if cp==2:
+        sp=num
+    else:
+        if cp==4:
+            ap=num
+    nv=nv+1
+    c=2
+if sp<ap:
+    spd=sp+1
+    while spd<=ap:
+        while cp<ap and l==0:
+            if spd%c==0:
+                l=0
+            else:
+                l=1
+        if l==0:
+            zp=zp+1
+        spd=spd+1
+    if zp==0:
+        print("el segundo y cuarto primo son consecutivos")
+    else:
+        print("no lo son")
+else:
+    apd=ap+1
+    while apd<=sp:
+        while cp<sp and l==0:
+            if apd%c==0:
+                l=0
+            else:
+                l=1
+        if l==0:
+            zp=zp+1
+        apd=apd+1
+    if zp==0:
+        print("el segundo y cuarto primo son consecutivos")
+    else:
+        print("no lo son")
+
+
+
+
+
+#SUMA FACTORIALES FIBONNACCI
+cd=int(input("ingrese la cantidad de numeros a sumas : "))
+nv=1
+r=1
+sf=0
+while nv<=cd:
+    num=int(input("ingrese el nummero : "))
+    a=0
+    b=1
+    t=0
+    while t<num:
+        t=a+b
+        a=b
+        b=t
+    if t==num:
+        while t>0:
+            r=r*t
+            t=t-1
+        sf=sf+r
+    r=1
+    nv=nv+1
+
+print(sf)
+
+
+
+
+
+#COMPARAR SUMA FAC PRIMOS Y FIBONNACCI
+cd=int(input("ingrese la cantidad de numeros : "))
+nv=1
+r=1
+sf=0
+sp=0
+cp=0
+cf=0
+c=1
+ce=0
+while nv<=cd:
+    num=int(input("ingrese el nummero : "))
+    a=0
+    b=1
+    t=0
+    while t<num:
+        t=a+b
+        a=b
+        b=t
+    if t==num:
+        while t>0:
+            r=r*t
+            t=t-1
+        sf=sf+r
+        cf=cf+1
+    while c<=num:
+        if num%c==0:
+            ce=ce+1
+        c=c+1
+    if ce==2:
+        while num>0:
+            r=r*num
+            num=num-1
+        sp=sp+r
+        cp=cp+1
+    nv=nv+1
+    r=1
+    ce=0
+    c=1
+print(sp,cp,sf,cf)
+pp=sp/cp
+pf=sf/cf
+if pp>pf:
+    print("el promedio de la suma de factoriales primos : ",pp," es mayor que la suma de factoriales fibbonnacci : ",pf)
+else:
+    print("el promedio de la suma de factoriales fibbonacci : ",pf," es mayor que la suma de factoriales primos : ",pp)
+
+
