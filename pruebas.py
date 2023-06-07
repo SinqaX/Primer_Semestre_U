20394beecaaa01fc765b6e99711b50965bbb034f
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-06-07 15:40:19 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,23 +1,4 @@
-resp=input("desea procesar triangulos ? (s/n) : ")
-while resp=='s'or resp=='S':
-    a=int(input("ingrese el valor a : "))
-    b=int(input("ingrese el valor b : "))
-    c=int(input("ingrese el valor c : "))
-    if a>0 and b>0 and c>0:
-        if a==b:
-            if b==c:
-                print("triangulo equilatero")
-            else:
-                print("triangulo isoceles")
-        else:
-            if a==c:
-                print("triangulo isoceles")
-            else:
-                if b==c:
-                    print("triangulo isoceles")
-                else:
-                    print("triangulo escaleno")
-    resp=input("desea procesar triangulos ? (s/n) : ")
+
 
 
     
