5637a891e5f42cad47eb0c78e1d03156154dc0e0
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-06-07 19:18:11 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,5 +1,20 @@
+#FIBONNACCI MENOR VECTOR
+v=[]
+t=int(input("ingrese la cantidad de datos del vector : "))
+cp=0
+men=0
+for val in range(t):
+    val=int(input("ingrese el valor : "))
+    v.append(val)
+for val in range(t):
+    if cp==0:
+        men=v[val]
+        cp=1
+    else:
+        if v[val]<men:
+            men=v[val]
+v.remove(men)
+v.insert(0, men)
 
-
-
-    
+print (v)
 
