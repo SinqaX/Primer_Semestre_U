210fa455db085b857cb04c0207bc10abe295c110
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-07-26 20:26:01 -0500

    Create prueba_taller.py

---
+++ prueba_taller.py
@@ -0,0 +1,55 @@
+#Ejercicios con vectores
+
+#18.	Se tiene un vector con datos numéricos donde hay varios Fibonacci, encontrar el segundo 
+# y tercer Fibonacci y sus posiciones, reemplazar las posiciones comprendidas en estos dos valores 
+# con el factorial del Fibonacci menor de los dos.
+a=[1,3,13,5,21,15,4,9]
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+
+    
+
+
+
+
+
