3cb3f51115647ed850b5e316b06f66850955f770
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-08-20 20:02:06 -0500

    Update prueba_taller.py

--- prueba_taller.py
+++ prueba_taller.py
@@ -1,30 +1,64 @@
-rang=[3,6]
-v2=[5,8,4,12,4,26,34,17,19,]
-j=0
-for i in range(len(rang)):
-    t=rang[i]
-    c=False
-    con=0
-    suma=0
-    while j<t:
-        if c==False:
-            men=v2[j]
-            may=v2[j]
-            c=True
+def fibbonaci(valor):
+    a=0
+    b=1
+    t=0
+    while t<valor:
+        t=a+b
+        a=b
+        b=t
+    if t==valor:
+        return(True)
+    else:
+        return(False)
+    
+def CrearMatriz():
+    m=[]
+    fm=int(input("ingrese la cantidad de filas de la matriz"))
+    cm=int(input("ingrese la cantidad de columnas de la matriz"))
+    for i in range(fm):
+        f=[]
+        for j in range(cm):
+            val=int(input("ingrese el dato :"))
+            f.append(val)
+        m.append(f)
+    return m
+                        
+def primo(valor):
+    r=2
+    p=False
+    if valor<=1:
+        return False
+    else:
+        while r<valor-1 and p==False:
+            if valor%r==0:
+                p=True
+            r+=1
+        if p==False:
+            return True
         else:
-            if v2[j]>may:
-                may=v2[j]
-            else:
-                if v2[j]<men:
-                    men=v2[j]
-        suma+=v2[j]
-        j+=1
-        con+=1
-    if con>0:
-        prom=suma/con
-    print("el mayor del rango ",i+1," es el ",may)
-    print("el menor del rango ",i+1," es el ",men)
-    print("el promedio del rango ",i+1," es ",prom)   
+            return False
+
+def listas(m):
+    lp=[]
+    lf=[]
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            if fibbonaci(m[i][j])==True:
+                lf.append(m[i][j])
+            if primo(m[i][j])==True:
+                lp.append(m[i][j])
+    return lp, lf
+
+m=CrearMatriz()
+lp,lf=listas(m)
+print("vector de fibbonaci :\n",lf)
+print("vector de primos : \n",lp)
+      
+
+
+
+
+
 
 
 
