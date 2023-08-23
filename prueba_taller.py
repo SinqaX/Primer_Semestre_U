09efe27c8b2d78aedfdf1a8c1e81bcd8ea8604cf
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-08-23 09:41:41 -0500

    Update prueba_taller.py

--- prueba_taller.py
+++ prueba_taller.py
@@ -1,51 +1,33 @@
-def punto4():
-    cd=int(input("ingrese la cantidad de datos "))
-    c=0
-    for i in range(cd):
-        num=int(input("ingrese el numero : "))
-        a=0
-        b=1
-        t=0
-        while t<num:
-            t=a+b
-            a=b
-            b=t
-        if t==num:
-            c+=1
-            if c==1:
-                f1=num
-            elif c==2:
-                f2=num
-            elif c==3:
-                f3=num
-    ma=f1
-    me=f1
-    if f2>ma:
-        ma=f2
-    elif f2<me:
-        me=f2
-    if f3>ma:
-        ma=f3
-    elif f3<me:
-        me=f3
-    sp=0
-    cp=0
-    p=0
-    mef=me
-    while me<ma:
-        if me%2==0:
-            sp+=me
-            cp+=1
-        me+=1
-    if cp>0:
-        p=sp/cp
-    return    print(f"el promedio de los numeros pares que estan entre el fibonacci menor {mef}, y el fibonacci mayor {ma}, es {p}")
+##se tiene una matriz cuadrada de orden n realizar lo siguiente 
+#-hallar el promedio diagonal prinpipal y secundadario
+m=[
+    [2,5,6,8],
+    [5,8,7,4],
+    [9,4,1,3],
+    [11,4,10,2]
+]
+def Promedio_diag_principal(m):
+    fm=len(m)
+    cm=len(m[0])
+    sup=0
+    for i in range(fm):
+        for j in range(cm):
+            if i==j :
+                sup+=m[i][j]
+    prop=sup/fm
+    return  print(f"el promedio de la daigonal principal es {prop}")
 
-
-while True:
-    dig=int(input("que punto quiere ejecutar ?"))
-    if dig==4:
-            punto4()
+def Promedio_diag_Secundaria(m):
+    fm=len(m)
+    cm=len(m[0])
+    sus=0
+    s=fm-1
+    for i in range(fm):
+        for j in range(cm):
+            if i+j== s:
+                sus+=m[i][j]
+    prop=sus/fm
+    return  print(f"el promedio de la daigonal secundaria es {prop}")
 
 
 
