1e7ce4d7dc528439141fa5dc162ddd610e8c3f4c
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-08-23 10:16:10 -0500

    Update prueba_taller.py

--- prueba_taller.py
+++ prueba_taller.py
@@ -30,6 +30,30 @@ def Promedio_diag_Secundaria(m):
     return  print(f"el promedio de la daigonal secundaria es {prop}")
 
 
+m=[
+    [2,5,6,8],
+    [5,8,7,4],
+    [9,4,1,3],
+    [11,4,10,2]
+]
+fm=len(m)
+cm=len(m[0])
+for i in range(fm):
+    for j in range(cm):
+         if i==j:
+            mei=i
+            mej=j
+            for k in range(i+1,fm):
+                for l in range(j+1,cm):
+                    if k==l:
+                        if m[k][l]<m[mei][mej]:
+                            mei=k
+                            mej=l
+                        break
+            m[i][j],m[mei][mej]=m[mei][mej],m[i][j]
+print("la diagonal principal ordenada ascendentemente queda asi")
+print(m)
+
 
 
 
