82ce5f981d00909c693c34240023869f2fe2fa2a
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-08-27 18:42:09 -0500

    Update prueba_taller.py

--- prueba_taller.py
+++ prueba_taller.py
@@ -1,58 +1,31 @@
-##se tiene una matriz cuadrada de orden n realizar lo siguiente 
-#-hallar el promedio diagonal prinpipal y secundadario
-m=[
-    [2,5,6,8],
-    [5,8,7,4],
-    [9,4,1,3],
-    [11,4,10,2]
-]
-def Promedio_diag_principal(m):
-    fm=len(m)
-    cm=len(m[0])
-    sup=0
-    for i in range(fm):
-        for j in range(cm):
-            if i==j :
-                sup+=m[i][j]
-    prop=sup/fm
-    return  print(f"el promedio de la daigonal principal es {prop}")
-
-def Promedio_diag_Secundaria(m):
-    fm=len(m)
-    cm=len(m[0])
-    sus=0
-    s=fm-1
-    for i in range(fm):
-        for j in range(cm):
-            if i+j== s:
-                sus+=m[i][j]
-    prop=sus/fm
-    return  print(f"el promedio de la daigonal secundaria es {prop}")
+# -	  Se tiene una cantidad dada de ternas (3 valores numéricos por terna) correspondientes 
+# a los lados de triángulos, determinar cuántos triángulos equiláteros, escalenos e isósceles 
+# se encuentran en estos triángulos y la suma de los triángulos isósceles Sin utilizar condiciones compuestas.
+ct=int(input("ingrese la cantidad de ternas a digitar : "))
+se=0
+si=0
+ses=0
+for i in range(ct):
+    print(f"\nTerna N° {i+1}")
+    a=int(input("ingrese el valor a : "))
+    b=int(input("ingrese el valor b : "))
+    c=int(input("ingrese el valor c : "))
+    if a == b == c:
+        se += 1
+    elif a == b or a == c or b == c:
+        si += 1
+    else:
+        ses += 1
+print(f"\ntriangulos equialteros : {se}")
+print(f"triangulos isoceles : {si}")
+print(f"triangulos escalenos : {ses}\n")
 
 
-m=[
-    [2,5,6,8],
-    [5,8,7,4],
-    [9,4,1,3],
-    [11,4,10,2]
-]
-fm=len(m)
-cm=len(m[0])
-for i in range(fm):
-    for j in range(cm):
-         if i==j:
-            mei=i
-            mej=j
-            for k in range(i+1,fm):
-                for l in range(j+1,cm):
-                    if k==l:
-                        if m[k][l]<m[mei][mej]:
-                            mei=k
-                            mej=l
-                        break
-            m[i][j],m[mei][mej]=m[mei][mej],m[i][j]
-print("la diagonal principal ordenada ascendentemente queda asi")
-print(m)
+                                
+
+
+
+        
 
 
 
