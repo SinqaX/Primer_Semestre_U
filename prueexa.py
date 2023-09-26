014c983c8f016b72d3c1f292203a59934f4e0f94
SinqaX <133186997+SinqaX@users.noreply.github.com>
2023-09-26 10:23:26 -0500

    Update prueexa.py

--- prueexa.py
+++ prueexa.py
@@ -1,41 +1,81 @@
-#ALGORITMO EXAMEN
-#se tienen dos vectores con datos numerico s, en el primer vector los pimos 2 y 3 determianan la cantidad de datos a 
-#recorrer en el segundo vector, hallar el proemedio de los paresque estan en estos dos rangos 
-pri=[1,3,2,4,7,2]
-pa=[1,2,3,4,6,7,8,9]
-c=0
-pri_2=0
-pri_3=0
-for i in range(len(pri)):
-    r=2
-    p= False
-    while r<pri[i]  and p==False:
-        if pri[i]%r==0:
-            p=True
-        r+=1
-    if p==False:
-        if c==2:
-            pri_2=pri[i]
-        else: 
-            if c==3:
-                pri_3=pri[i]
-        c+=1
-print(pri_2,pri_3)
+#se tienen dos listas con datos numericos forar una tercera lista on lo primos no comunes sin repetidos 
+#y otra lista con lo s impares comenes sin repetidos la primera lista ordenada ascendente y la otra descendente 
+lista_1=[1,5,2,7,11,13]
+lista_2=[19,5,4,7,3,3,5]
+lista_primos=[]
+lista_impares=[]
+def es_primo(valor):
+        r=2
+        p=False
+        if valor<=1:
+            return False
+        else:
+            while r<valor-1 and p==False:
+                if valor%r==0:
+                    p=True
+                r+=1
+            if p==False:
+                return True
+            else:
+                return False    
+def es_impar(dato):
+    if dato % 2 == 0:
+        return False
+    else:
+        return True
+def quick_sort_ascending(lista, inicio, fin):
+    if inicio > fin:
+        return
+    anterior = inicio
+    posterior = fin
+    pivo = lista[inicio]
 
-if pri_2<pri_3:
-    lim_inf=pri_2
-    lim_sup=pri_3
-else:
-    lim_inf=pri_3
-    lim_sup=pri_2
-sump=0
-cp=0
-j=lim_inf
-for j in range(lim_sup):
-    if pa[j]%2==0:
-        sump+=pa[j]
-        cp+=1
-if cp>0:
-    prom=sump/cp
-print("el promedio de numeros pares entre los rangos ",lim_inf," y ",lim_sup," es : ",prom)
+    while anterior < posterior:
+        while anterior < posterior and lista[posterior] > pivo:
+            posterior = posterior - 1
+
+        if anterior < posterior:
+            lista[anterior] = lista[posterior]
+            anterior = anterior + 1
+
+        while anterior < posterior and lista[anterior] <= pivo:
+            anterior = anterior + 1
+
+        if anterior < posterior:
+            lista[posterior] = lista[anterior]
+            posterior = posterior - 1
+
+        lista[anterior] = pivo
+
+    quick_sort_ascending(lista, inicio, anterior - 1)
+    quick_sort_ascending(lista, anterior + 1, fin)
+def descendente(lista):
+    for i in range(len(lista)):
+        me=i
+        for j in range(i+1, len(lista)):
+            if lista[j]>lista[me]:
+                me=j
+        lista[i],lista[me]=lista[me],lista[i]
+
+for i in range(len(lista_1)):
+    if es_primo(lista_1[i]):
+        if lista_1[i] not in lista_2:
+            if lista_1[i] not in lista_primos:
+                lista_primos.append(lista_1[i])           
+    if es_impar(lista_1[i]):
+        if lista_1[i] in lista_2:
+            if lista_1[i] not in lista_impares:
+                lista_impares.append(lista_1[i])
+for p in range(len(lista_2)):
+    if es_primo(lista_2[p]):
+        if lista_2[p] not in lista_1:
+            if lista_2[p] not in lista_primos:
+                lista_primos.append(lista_2[p]) 
+print(f"lista de primos : {lista_primos}")
+print(f"lista de impares :{lista_impares}")
+quick_sort_ascending(lista_primos, 0, len(lista_primos)-1)
+descendente(lista_impares)
+print(f"la lista de primos ordenada ascendente queda : {lista_primos}")
+print(f"la lista de impares ordenada descendente queda : {lista_impares}")
+
 
